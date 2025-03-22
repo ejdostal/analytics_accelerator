22bf8447e8885ad1553a50be06d19acb5e869f4c
Erica Dostal <ejdostal@gmail.com>
2025-03-22 16:13:43 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -4,9 +4,10 @@ Clauses, Commands / Statements:
 - SELECT: choose columns (See all columns with *)
 - FROM: choose tables
 - ORDER BY: sorts your results using the data in any column
+  - Useful when you want to sort orders by date, for example
   - When you use ORDER BY in a SQL query, your output will be sorted that way only temporarily. The next query you run will encounter the unsorted data again. 
   - It's important to keep in mind that this is different than using common spreadsheet software, where sorting the spreadsheet by column actually alters the data in that sheet until you undo or change that sorting. This highlights the meaning and function of a SQL "query".
-  - DESC can be added after the column in your ORDER BY statement to sort in descending order. (The default is to sort in ascending order.)
+  - DESC can be added after the column in your ORDER BY statement to sort in descending order. (The default is to sort in ascending order: A to Z, lowest to highest, or earliest to latest.)
 - LIMIT: see just the first few rows of the table.
   - Useful when you want to see just the first few rows of a table. This can be much faster for loading than if we load the entire dataset. 
   - The LIMIT command is always the very last part of a query.
@@ -42,3 +43,23 @@ SELECT occurred_at, account_id, channel
 FROM web_events
 LIMIT 15;
   -- shows only these 3 columns from the web_events table, limited to only the first 15 rows of data. -- 
+
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at
+LIMIT 10;
+  -- returns the these 3 columns for the 10 earliest orders in the orders table (sorts the data from lowest to highest based on the occurred_at column, then limits to only the first 10 rows of data.) --
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 5;
+  -- returns these 3 columns for the 5 largest orders in terms of sales (total_amt_usd). --
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 20;
+  -- returns these 3 columns for the 20 smallest orders in terms of sales (total_amt_usd). --
+
+
