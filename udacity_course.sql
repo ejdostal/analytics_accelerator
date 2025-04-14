faf7d201393582e136d3d2c0aa6bbca8a628c84c
Erica Dostal <ejdostal@gmail.com>
2025-04-14 13:48:12 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -955,3 +955,26 @@ ORDER BY account_id
 -- DISTINCT comes immediately after the SELECT clause.
 
 
+-- Tests if there are any accounts associated with more than one region. --    
+    SELECT DISTINCT id, name
+    FROM accounts;
+    -- returns every unique row for the id and name columns in the accounts table; returns each only once.
+    -- ex. returns 351 results
+    
+    SELECT a.id as "account id", r.id as "region id", 
+    a.name as "account name", r.name as "region name"
+    FROM accounts a
+    JOIN sales_reps s
+    ON s.id = a.sales_rep_id
+    JOIN region r
+    ON r.id = s.region_id;
+    -- returns all associated accounts. --
+    -- ex. returns 351 results; each account is associated with only one region. 
+    -- If the SELECT DISTINCT query had returned more rows than the second query, that means that some accounts could be associated with more than one region. 
+
+
+
+
+
+
+
