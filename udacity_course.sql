af1828484eed933f0e037960328498af12eff480
Erica Dostal <ejdostal@gmail.com>
2025-04-18 10:00:33 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -1433,6 +1433,64 @@ FROM orders)
 -- Finds only the orders that took place in the same month and year as the first order, and then pulls the average for each type of paper qty in this month; includes the total amount spent on all orders during this month as well. 
 
 
+-- Subquery Mania -- 
+
+-- Provides the name of the sales_rep in each region with the largest amount of total_amt_usd sales. --
+
+	-- Step 1 --
+	SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+	FROM sales_reps s
+	JOIN accounts a
+	ON a.sales_rep_id = s.id
+	JOIN orders o
+	ON o.account_id = a.id
+	JOIN region r
+	ON r.id = s.region_id
+	GROUP BY 1,2
+	ORDER BY 3 DESC;
+	-- Returns the total sales associated with each sales_rep, along with their names and region. --
+
+	-- Step 2 --
+	SELECT region_name, MAX(total_amt) total_amt
+        FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+                FROM sales_reps s
+                JOIN accounts a
+                ON a.sales_rep_id = s.id
+                JOIN orders o
+                ON o.account_id = a.id
+                JOIN region r
+                ON r.id = s.region_id
+                GROUP BY 1, 2) t1
+        GROUP BY 1;
+	-- Outer query pulls only the maximum total sales for each region from the previous results. --
+
+
+	-- Step 3 -- 
+	SELECT t3.rep_name, t3.region_name, t3.total_amt
+	FROM(SELECT region_name, MAX(total_amt) total_amt
+	        FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+	                FROM sales_reps s
+	                JOIN accounts a
+	                ON a.sales_rep_id = s.id
+	                JOIN orders o
+	                ON o.account_id = a.id
+	                JOIN region r
+	                ON r.id = s.region_id
+	                GROUP BY 1, 2) t1
+	        GROUP BY 1) t2
+	JOIN (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+	        FROM sales_reps s
+	        JOIN accounts a
+	        ON a.sales_rep_id = s.id
+	        JOIN orders o
+	        ON o.account_id = a.id
+	        JOIN region r
+	        ON r.id = s.region_id
+	        GROUP BY 1,2
+	        ORDER BY 3 DESC) t3
+	ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt;
+
+	
 	
 	
 SELECT s.name rep, r.name region, SUM(o.total_amt_usd) total_sales
