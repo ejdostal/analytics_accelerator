0fe1be51916631670f3e51ebfda75a4266deb7df
Erica Dostal <ejdostal@gmail.com>
2025-05-04 15:17:42 -0700

    Update sql_data_cleaning.sql
    added overview summary

--- sql_data_cleaning.sql
+++ sql_data_cleaning.sql
@@ -1,1 +1,7 @@
+/* SQL Data Cleaning Techniques - Get from raw data to clean data that's useful for analysis.
+
+1. Clean and re-structure messy data.
+2. Convert columns to different data types.
+3. Tricks for manipulating NULLs. */
+
 
