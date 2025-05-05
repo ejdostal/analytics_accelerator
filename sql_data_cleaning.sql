07882480cbb648ead3f1184e50e90649db6575ff
Erica Dostal <ejdostal@gmail.com>
2025-05-05 08:37:35 -0700

    Update sql_data_cleaning.sql

--- sql_data_cleaning.sql
+++ sql_data_cleaning.sql
@@ -11,19 +11,20 @@
 - RIGHT pulls a specified number of characters for each row in a specified column starting at the end (or from the right)
 - LENGTH provides the number of characters for each row of a specified column; returns the length of the string. */
 
-/* Queries:
+Queries:
+--------
   
 Pulls area code, phone number without area code (twice, in two different ways) for each customer name. */ 
 
 SELECT first_name,
   last_name,
   phone_number
-  LEFT(phone_number, 3) AS area_code,       /* pulls the area code out of the phone number. */
-  RIGHT(phone_number, 8) AS phone_number_only,     /* pulls the entire phone number, with hyphen, but without area code. */
-  RIGHT(phone_number, LENGTH(phone_number) - 4) AS phone_number_alt     /* you can also represent the RIGHT function as a function of the length: the full phone number (12 characters) - the area code, with hyphen (4 characters) =  the phone number, with hyphen, without area code. */
+  LEFT(phone_number, 3) AS area_code,       -- pulls the area code out of the phone number.
+  RIGHT(phone_number, 8) AS phone_number_only,     -- pulls the entire phone number, with hyphen, but without area code.
+  RIGHT(phone_number, LENGTH(phone_number) - 4) AS phone_number_alt     -- you can also represent the RIGHT function as a function of the length: the full phone number (12 characters) - the area code, with hyphen (4 characters) =  the phone number, with hyphen, without area code.
 FROM customer_data 
   
-/* Provides the type of web address (or extension) each company is using for their website and provides how many of each website type exist in the accounts table. */
+-- Provides the type of web address (or extension) each company is using for their website and provides how many of each website type exist in the accounts table. 
 
 SELECT RIGHT(website, 3) AS domain, 
   COUNT(*) num_companies
@@ -31,35 +32,35 @@ FROM accounts
 GROUP BY 1
 ORDER BY 2 DESC;
 
-/* Pulls the first character of each company name to see the distribution of company names that begin with each letter (or number). */
+-- Pulls the first character of each company name to see the distribution of company names that begin with each letter (or number).
 
-SELECT LEFT(UPPER(name), 1) AS first_letter, COUNT(*) num_companies    /* UPPER ensures that both upper and lowercase versions of the same letter end up in the same group. */
+SELECT LEFT(UPPER(name), 1) AS first_letter, COUNT(*) num_companies    -- UPPER ensures that both upper and lowercase versions of the same letter end up in the same group. 
 FROM accounts
 GROUP BY 1
 ORDER BY 2 DESC;
 
-/* Separates company names into two groups: one where company names start with a number, another where company names start with a letter. Then finds the proportion of company names that start with a letter vs. starts with a number. */
+-- Separates company names into two groups: one where company names start with a number, another where company names start with a letter. Then finds the proportion of company names that start with a letter vs. starts with a number. 
 
 WITH t1 AS ( 
 SELECT name, 
 CASE WHEN LEFT(UPPER(name), 1) IN('0','1','2','3','4','5','6','7','8','9') 
-    THEN 1 ELSE 0 END AS num,      /* in the "num" group, company names that start with a number return 1, names that start with a letter return 0. */
+    THEN 1 ELSE 0 END AS num,      -- in the "num" group, company names that start with a number return 1, names that start with a letter return 0. 
 CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
-    THEN 0 ELSE 1 END AS letter    /* in the "letter" group, company name names that start with a number return 0, names that start with a letter return 1. */
+    THEN 0 ELSE 1 END AS letter    -- in the "letter" group, company name names that start with a number return 0, names that start with a letter return 1. 
 FROM accounts
 )
   
 SELECT SUM(num) nums, SUM(letter) letters 
-FROM t1;     /* in the "nums" column, all values in the "num" group are summed. (company names starting with a number returned 1's) */
-             /* in the "letters" column, all values in the "letter" group are summed. (company names starting with a letter  returned 1's) */
+FROM t1;     -- in the "nums" column, all values in the "num" group are summed. (company names starting with a number returned 1's)
+             -- in the "letters" column, all values in the "letter" group are summed. (company names starting with a letter  returned 1's)
 
 
-/* Finds the proportion of company names starting with a vowel vs. those starting with something else (consonant). */
+-- Finds the proportion of company names starting with a vowel vs. those starting with something else (consonant). 
 
-SELECT SUM(vowels) vowels, SUM(other) others   /* In the "vowels" column, all 1's in the "vowel" group are summed up, in the "others" column all 1's in the "others" group are summed. */ 
+SELECT SUM(vowels) vowels, SUM(other) others   -- In the "vowels" column, all 1's in the "vowel" group are summed up, in the "others" column all 1's in the "others" group are summed.
 FROM (SELECT name, 
   CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
-      THEN 1 ELSE 0 END AS vowels,     /* in the "vowels" group, company names that start with a vowel return 1, names that DON'T start with a vowel return 0. */
+      THEN 1 ELSE 0 END AS vowels,    -- in the "vowels" group, company names that start with a vowel return 1, names that DON'T start with a vowel return 0. 
   CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
-      THEN 0 ELSE 1 END AS other       /* in the "other" group, company names that DON'T start with a vowel return 1, names that start with a vowel 0. */
+      THEN 0 ELSE 1 END AS other      -- in the "other" group, company names that DON'T start with a vowel return 1, names that start with a vowel 0. 
 FROM accounts) t1;
